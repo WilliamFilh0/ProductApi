c7fafe9080f9e0c950b0b74de645e32ab6d25cf0
WilliamFilh0 <williamfilho8908@gmail.com>
2024-12-09 14:31:36 -0300

    style: format code

--- src/main/java/com/teste/product/repository/ProdutoRepository.java
+++ src/main/java/com/teste/product/repository/ProdutoRepository.java
@@ -1,6 +1,5 @@
 package com.teste.product.repository;
 
-
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -17,6 +16,7 @@ public class ProdutoRepository {
 
   /**
    * Metodo para retornar uma lista de produtos.
+   * 
    * @return Lista de produtos.
    */
   public List<Produto> obterTodos() {
@@ -25,8 +25,9 @@ public class ProdutoRepository {
 
   /**
    * Metodo que retorna o produto encontrado pelo seu Id.
+   * 
    * @param id do produto que ser localizado.
-   * @return Retorna um produto caso seja encontrado. 
+   * @return Retorna um produto caso seja encontrado.
    */
   public Optional<Produto> obterPorId(Integer id) {
     return produtos
@@ -37,10 +38,11 @@ public class ProdutoRepository {
 
   /**
    * Metodo para adcioanr produto na lista.
-   * @param produto que sera adicionado. 
+   * 
+   * @param produto que sera adicionado.
    * @return Retorna o produto que foi adicionado na lista.
    */
-  
+
   public Produto adicionar(Produto produto) {
     ultimoId++;
 
@@ -52,6 +54,7 @@ public class ProdutoRepository {
 
   /**
    * Metodo para deletar o produto por id;
+   * 
    * @param id do produto a ser deletado;
    */
 
@@ -61,23 +64,24 @@ public class ProdutoRepository {
 
   /**
    * Metodo para atualizar o produto na lista.
+   * 
    * @param produto que será atualizado.
    * @return retrona o produto após atualizar na lista.
    */
 
   public Produto atualizar(Produto produto) {
-    //Encontrar o produto na lista. 
+    // Encontrar o produto na lista.
     Optional<Produto> produtoEncontrado = obterPorId(produto.getId());
-   
-    //Verificar se encontrou.
+
+    // Verificar se encontrou.
     if (produtoEncontrado.isEmpty()) {
       throw new InputMismatchException("Produto não encontrado");
     }
-    
-    //Remover o produto antigo da lista.
+
+    // Remover o produto antigo da lista.
     deletar(produto.getId());
 
-    //Depois adicionar o produto atualizado na lista.
+    // Depois adicionar o produto atualizado na lista.
     produtos.add(produto);
 
     return produto;
